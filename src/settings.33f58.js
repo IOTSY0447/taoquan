7f2e538a26d332bf379af77552014805df5f3c1b
lzl <354244489@qq.com>
2020-12-09 14:54:06 +0800

    no message

---
+++ src/settings.33f58.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"f1413",resources:"49ede",main:"8fa64"}};
